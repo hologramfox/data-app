b2c937e45c759c241c59bfbb42959ddd0848476b
hologramfox <56520147+hologramfox@users.noreply.github.com>
2021-05-15 06:43:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 #from fastapi.staticfiles import StaticFiles
 #from fastapi.templating import Jinja2Templates
+import pymongo
 
 import json
 from pydantic import BaseModel

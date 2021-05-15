826344a726cb846fca0e4d16adad52266c7dab63
hologramfox <offboxm@gmail.com>
2021-05-15 18:22:33 +0300

    released

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import uvicorn
 from fastapi import FastAPI
 #from fastapi.staticfiles import StaticFiles
 #from fastapi.templating import Jinja2Templates
-import pymongo
+# import pymongo
+import psycopg2
 
 import json
 from pydantic import BaseModel
@@ -42,17 +43,30 @@ class ImageScore(BaseModel):
 #         json.dump(dic, fwrite)
 #     return "ok"
 
+# def getScore(image_score: ImageScore):
+#     client = MongoClient("mongodb+srv://input_user:QceG9PBO27FiDFoi@cluster0.hex3g.mongodb.net/trialSave?retryWrites=true&w=majority")
+#     db = client.test
+#     collection = db.testcol
+#     numbers = db.numbers
+#     stat_dict = {"image_name":f"image_{str(image_score.imageNum)}", "image_score":[image_score.scoreNum]}
+#     # stat_dict[str(image_score.imageNum)] = [image_score.scoreNum]
+
+#     result = numbers.insert_one(stat_dict)
+
+#     return "ok"
 def getScore(image_score: ImageScore):
-    client = MongoClient("mongodb+srv://input_user:QceG9PBO27FiDFoi@cluster0.hex3g.mongodb.net/trialSave?retryWrites=true&w=majority")
-    db = client.test
-    collection = db.testcol
-    numbers = db.numbers
-    stat_dict = {"image_name":f"image_{str(image_score.imageNum)}", "image_score":[image_score.scoreNum]}
-    # stat_dict[str(image_score.imageNum)] = [image_score.scoreNum]
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
 
-    result = numbers.insert_one(stat_dict)
+    cur.execute("CREATE TABLE answers (id serial PRIMARY KEY, imagenum integer, answ varchar);")
+    cur.execute("INSERT INTO answers (imagenum, answ) VALUES (%s, %s)", (image_score.imageNum, str(image_score.scoreNum)))
+    conn.commit()
 
+    cur.close()
+    conn.close()
     return "ok"
+
 
 @app.post("/saveScore")
 def saveScore(imageScore: ImageScore):
